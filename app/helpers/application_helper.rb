09bde8d0344a9e08d82357019bf2ba0b84272a11
SouthWolf <wp.southwolf@gmail.com>
2011-12-07 10:47:15 +0800

    User signup complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,16 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
 	end
+
+  def notice_message
+    flash_messages = []
+
+    flash.each do |type, message|
+      type = :success if type == :notice
+      text = content_tag(:div, link_to("x", "#", :class => "close") + message, :class => "alert-message #{type} fade in", "data-alert" => "alert")
+      flash_messages << text if message
+    end
+
+    flash_messages.join("\n").html_safe
+  end
 end
---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe "Users" do
+  describe "signup" do
+    
+    describe "failure" do
+    
+      it "should not make a new user" do
+        lambda do
+          visit signup_path
+          fill_in "Name",         :with => ""
+          fill_in "Email",        :with => ""
+          fill_in "Password",     :with => ""
+          fill_in "Confirmation", :with => ""
+          click_button "Sign up"
+          page.should have_selector("h1", :content => "Sign up")
+          page.should have_selector("p.error_notification")
+        end.should_not change(User, :count)
+      end
+    end
+
+    describe "success" do
+      it "should make a new user" do
+        lambda do
+          visit signup_path
+          fill_in "Name",         :with => "Example User"
+          fill_in "Email",        :with => "user@example.com"
+          fill_in "Password",     :with => "foobar"
+          fill_in "Confirmation", :with => "foobar"
+          click_button "Sign up"
+          page.should have_selector("div.alert-message.success",
+                                        :content => "Welcome")
+          page.should have_selector("img.gravatar")
+        end.should change(User, :count).by(1)
+      end
+    end
+  end
+end
